067c601bc8712f36c6ec083d8986b0e4086e7083
Topik <topikmujianto7@gmail.com>
2019-02-22 23:18:55 +0700

    Initial Commit

---
+++ TestTravisCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTravisCI
+//
+//  Created by Topik Mujianto on 22/02/19.
+//  Copyright © 2019 Topik Mujianto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
